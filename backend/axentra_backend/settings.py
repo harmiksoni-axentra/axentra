5ca4a042fcc313b5b39a0d1daadd221dfd489958
roopesh-digiwagon <roopesh.rai@digiwagon.com>
2026-05-08 15:52:19 +0530

    axentra changes

--- backend/axentra_backend/settings.py
+++ backend/axentra_backend/settings.py
@@ -105,6 +105,6 @@ EMAIL_BACKEND = os.getenv(
 )
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "harmiksoni@gmail.com")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "qusmskczlsmyuwye")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "axentrallc@gmail.com")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "nrqqvdpergfnkzno")
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "true").lower() == "true"
