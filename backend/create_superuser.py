a92ea2e2b3bf40fd541974eae340e4f3eb7e096c
roopesh-digiwagon <roopesh.rai@digiwagon.com>
2026-05-08 15:59:19 +0530

    createsuperuser

--- backend/create_superuser.py
+++ backend/create_superuser.py
@@ -3,7 +3,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "acentra.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "axentra_backend.settings")
 # 👆 replace "acentra.settings" with your actual project settings path if different
 
 django.setup()
